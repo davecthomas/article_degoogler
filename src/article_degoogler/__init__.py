c9139bd97a14774111663af24e1eb444c9a09914
Dave Thomas <2355287+davecthomas@users.noreply.github.com>
2025-06-19 10:39:03 -0700

    Fix redirect cleanup and update package

--- src/article_degoogler/__init__.py
+++ src/article_degoogler/__init__.py
@@ -2,6 +2,6 @@
 
 __all__ = ["clean_href", "process_html", "process_file", "main", "__version__"]
 
-from .fix_links import clean_href, process_html, process_file, main
+from .article_degoogler import clean_href, process_html, process_file, main
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
